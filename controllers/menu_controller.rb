0f6e18bc2a796731cda07b8cad1b82a2ae5578a4
Curtis Wilson <curtisadmin@DESKTOP-A7OEB5Q.localdomain>
2018-01-18 10:19:05 -0700

    Add submenu, entry create

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -25,7 +25,7 @@ class MenuController
                 view_all_entries
                 main_menu
             when 2
-                systsem "clear"
+                system "clear"
                 create_entry
                 main_menu
             when 3
@@ -47,9 +47,33 @@ class MenuController
         end
 
         def view_all_entries
+
+            address_book.entries.each do |entry|
+                system "clear"
+                puts entry.to_s
+
+                entry_submenu(entry)
+            end
+
+            system "clear"
+            puts "end of entries"
         end
 
         def create_entry
+
+            system "clear"
+            puts "New AddressBloc Entry"
+            print "name: "
+            name = gets.chomp
+            print "Phone Number: "
+            phone = gets.chomp
+            print "Email: "
+            email = gets.chomp
+
+            address_book.add_entry(name,phone,email)
+            system "clear"
+            puts "New Entry Created"
+
         end
 
         def search_entries
@@ -58,4 +82,31 @@ class MenuController
         def read_csv
             
     end
+
+    def entry_submenu(entry)
+
+        puts "n - next entry"
+        puts "d - delete entry"
+        puts "e - edit  this entry"
+        puts "m - return to main menu"
+
+        selection = gets.chomp
+        
+        case selection
+            when "n"
+
+            when "d"
+            when "e"
+
+            when "m"
+                system "clear"
+                main_menu
+            else
+                system "clear"
+                puts "#{selection} is not a valid input"
+                entry_submenu(entry)
+         end
+     end
+    
+    
 end
